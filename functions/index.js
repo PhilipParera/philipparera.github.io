046542e59890712107cb74433b883c0cc68593cd
Julius Philip Timotius (PUR-JKT) <freight.ebidding@gmail.com>
2025-04-03 07:01:59 +0700

    Fix index.js

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,14 @@
 exports.authenticateBidder = async (req, res) => {
-  res.set('Access-Control-Allow-Origin', 'https://www.freight-ebidding.com');
-  res.set('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.set('Access-Control-Allow-Headers', 'Content-Type');
-  if (req.method === 'OPTIONS') {
-    return res.status(204).send('');
+  try {
+    res.set('Access-Control-Allow-Origin', 'https://www.freight-ebidding.com');
+    res.set('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') {
+      return res.status(204).send('');
+    }
+    return res.status(200).send({ message: 'Test successful' });
+  } catch (error) {
+    console.error('Error in function:', error);
+    return res.status(500).send({ error: 'Internal server error' });
   }
-  return res.status(200).send({ message: 'Test successful' });
 };
